6f94ee2fd3eec36df75c333b9b59b9932db6665f
Juhász Dániel <juhasz.daniel@chaos-solutions.hu>
2022-05-07 01:46:37 +0200

    Bejelentkezés

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {
+  MatSnackBarModule,
+  MAT_SNACK_BAR_DEFAULT_OPTIONS,
+} from '@angular/material/snack-bar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,8 +24,18 @@ import { NotFoundPageComponent } from './not-found-page/not-found-page.component
     BrowserAnimationsModule,
     MatCardModule,
     MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    HttpClientModule,
+    FormsModule,
+    MatSnackBarModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
+      useValue: { duration: 5000, panelClass: 'snack-bar-error' },
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,15 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent implements OnInit {
+  username = '';
+  password = '';
 
-  constructor() { }
+  constructor(
+    private readonly http: HttpClient,
+    private readonly snackBar: MatSnackBar
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+    this.http
+      .post<{ username: string; type: string }>('/api/login', {
+        username: this.username,
+        password: this.password,
+      })
+      .subscribe({
+        next: (res) => console.log(res),
+        error: (err) =>
+          this.snackBar.open(err.error.error || err.message || 'Hiba!'),
+      });
   }
-
 }
